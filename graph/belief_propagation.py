6796a0130e4869ecfab9cb7b91e727d1026a2f47
Priyansh Trivedi <pc.priyansh@gmail.com>
2017-06-09 22:26:09 +0200

    *UNTESTED* Transformed the Belief Prop to work with theano symbolic variables instead of strings (talk about promotions, eh). It's late enough in the night to not test them.
    TODO: @geraltofrivia, do write a testbench for the bp python file.

--- graph/belief_propagation.py
+++ graph/belief_propagation.py
@@ -2,6 +2,7 @@
 	Authored: 8th June (first version)	
 '''
 from pprint import pprint
+import theano.tensor as T
 import numpy as np
 
 import node
@@ -31,6 +32,8 @@ class Graph:
 		
 		#Keep the list of variables in order to map the indices of the matrix to actual graph nodes
 		self.variables = _variables + [null_node]
+		#Keep the constant appearing in the i position of the head variable to be used while doing the BP 
+		self.u_c = None 	
 
 		#Keep the predicate that appeared in the head of the query in order to know where do we start the 
 		self.head_predicate = _fictional_factor
@@ -92,17 +95,23 @@ class Graph:
 
 
 
-	def propagate_thy_beliefs(self):
+	def propagate_thy_beliefs(self, _u_c):
 		'''
 			Call this function to receive a string containing the path of the belief propagation algorithm.
 			We implement the algorithm listed in the paper mentioned in the comments above
+			
+			Input: the constant appearing in the first position of the head predicate. 
 
 			Pseudocode:
+				-> Create an empty theano vector whose definitions will be iteratively changed.
 				-> Call compile_message_node_to_factor from the o node of the head predicate. 
 				-> Let the functions recursively call each other
 				-> Collect their things somehow. @TODO: how. what format. Shall we use theano variables altogether or what
 				-> Return said stuff.
 		'''	
+
+		self.u_c = _u_c
+
 		print "graph:bp: Starting belief propagation."
 		equation = self._comiple_message_node_(self.head_predicate.o, "Fictional Label")
 		print equation
@@ -129,15 +138,24 @@ class Graph:
 
 		if _node == self.head_predicate.i:
 			#This is the input variable.
-			return "u_"+_node.label
+			return self.u_c
 
 		#This is NOT the input variable.
 		neighbors = self._get_neighbours(_node, _exclude=_factor) #Will be a list of factors.
 		
 		#Send the neighbour + current node to compilemessage_factor and collect what they have to say.
-		v_x = ' \circle '.join([ self._compile_message_factor_(_factor = factor, _node = _node) for factor in neighbors ])
+		# v_x = ' \circle '.join([ self._compile_message_factor_(_factor = factor, _node = _node) for factor in neighbors ])
+		neighboring_values = [ self._compile_message_factor_(_factor = factor, _node = _node) for factor in neighbors ]
+		if len(neighboring_values) > 0:
+			v_x = neighboring_values[0]
+			for remaining_values in neighboring_values[1:]:
+				v_x = v_x * remaining_values
+		else:
+			#In this case, since there are no neighbors, there's literally nothing to return.
+			#@TODO: What do we do here
+			pass
 
-		return v_x		
+		return v_x
 
 	def _compile_message_factor_(self, _factor, _node):
 		'''
@@ -158,14 +176,16 @@ class Graph:
 		
 		if _factor.o.null:
 			#If the factor is unary.
-			return "v_"+factor.label
+			return _factor.M 	#@TODO: See if we need another variable like self.v to represent the value of unary predicates.
 
 		elif _factor.o == _node:
 			#If the node is the output node for this factor
 			v_i = self._comiple_message_node_(_factor.i, _factor)
-			return v_i+ " \dot M_"+_factor.label
+			# return v_i+ " \dot M_"+_factor.label
+			return v_i.dot(_factor.label)
 
 		elif _factor.i == _node:
 			 #If the node is the input node for this factor
 			v_i = self._comiple_message_node_(_factor.o, _factor)
-			return v_i+ " \dot M_"+_factor.label
+			# return v_i+ " \dot M_"+_factor.label
+			return v_i.dot(_factor.label)
