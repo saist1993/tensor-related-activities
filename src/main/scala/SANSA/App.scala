38f199db302f99e8ab2a9bc5cc7cb76ec8e58e18
Priyansh Trivedi <pc.priyansh@gmail.com>
2017-07-18 15:57:02 +0200

    The FactorGraph class now generates a Mxnet executor object after integrating everything, given a bunch of factors and variables.

--- src/main/scala/SANSA/App.scala
+++ src/main/scala/SANSA/App.scala
@@ -3,7 +3,7 @@ package SANSA
 import ml.dmlc.mxnet._
 import ml.dmlc.mxnet.{Symbol => s}
 import ml.dmlc.mxnet.{NDArray => nd}
-
+import ml.dmlc.mxnet.module.Module
 import SANSA.graph.{Variable => v}
 import SANSA.graph.{Factor => f}
 import SANSA.FactorGraph
@@ -15,30 +15,79 @@ object App {
 
 	//  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
 
-	def main(args : Array[String]) {
+	def main(args: Array[String]) {
 	}
+
 	//
 	//  val x = s.Variable(name = "a")
 	//  val w = s.Variable(name = "b")
+	val numberOfEntities = 10
 
-	val vara = new v(false, false, s.Variable("a"), "a")
+	val vara = new v(false, false, s.Variable("a", Map("type" -> "variable")), "a")
 	//
-	var vl = List(vara,new v(false, false, _u = s.Variable("b"), "b"),new v(false, false, _u = s.Variable("c"),"c"))
-	var f1 = new f(_i = vara, _o = vl(1), _M = s.Variable("f1"), _label = "f1")
-	var f2 = new f(_i = vara, _o = vl(2), _M = s.Variable("f2"), _label = "f2")
-	var f3 = new f(_i = vl(2), _o = vl(1), _M = s.Variable("f3"), _label = "f3")
+	var vl = List(vara, new v(false, false, _u = s.Variable("b", Map("type" -> "variable")), "b"), new v(false, false, _u = s.Variable("c",  Map("type" -> "variable")), "c"))
+	var f1 = new f(_i = vara, _o = vl(1), _M = s.Variable("f1", Map("type" -> "factor")), _label = "f1")
+	var f2 = new f(_i = vara, _o = vl(2), _M = s.Variable("f2", Map("type" -> "factor")), _label = "f2")
+	var f3 = new f(_i = vl(2), _o = vl(1), _M = s.Variable("f3", Map("type" -> "factor")), _label = "f3")
 
-	var g = new FactorGraph(_variables = vl, _factors_body = List(f2,f3), _factor_head = f1)
+	var g = new FactorGraph(_variables = vl, _factors_body = List(f2, f3), _factor_head = f1, _n_e = numberOfEntities)
 	//  var n = g.getNeighbors(vl(2),exclude=f2)
 
 	//  println(n(0)._label)
 	//	var u = s.Variable("u")
 
 	println("Jello")
-	var op: Symbol = g.beliefPropagation()
-	println(op.listArguments())
+	var op = g.beliefPropagation()
 
-//	var a = List(1,2,3)
-//	println(a(4))
+//	println(op.listArguments())
+//	println("op is:\t" + op.toString())
+//
+//
+//	val r1 = nd.ones(10, 10)
+//	val r2 = nd.ones(10, 10)
+//	val e1 = nd.array(Array(0,0,1,0,0,1,1,0,0,1), shape = Shape(1,10))
+//	val dr1 = nd.empty(10, 10)
+//	val dr2 = nd.empty(10, 10)
+//	val de1 = nd.empty(1, 10)
 
+
+	////////////////////////////////////////////////BIND!///////////////////
+	//Encapuslate op in a function
+	//do a fwd and bkwd pass (or atleast compute gradients and loss expressions)
+	//	var model = new Module(op)
+
+	//	val exe = op.bind(ctx = Context.cpu(), args = Map("a" -> e1, "f2" -> r1, "f3" -> r2), argsGrad = Map("f1" -> dr1, "f2" -> dr2))
+	//	exe.forward()
+	//	println(exe.outputs)
+	//	println(exe.outputs(0).shape)
+	//	println(exe.outputs.length)
+	////	model.bind(dataShapes = IndexedSeq(1,100),labelShapes = 1,)
+	//
+	////	var a = List(1,2,3)
+	////	println(a(4))
+	//	var gr1 = nd.ones(1,100)
+	//	var gr2 = nd.ones(1,100)
+	//	exe.backward( outGrads = Array(gr1, gr2 ))
+
+	// Simple Bind
+//	val exe = op.simpleBind(ctx = Context.cpu(), gradReq = "write", shapeDict = Map("a" -> Shape(1, 10), "f2" -> Shape(10, 10), "f3" -> Shape(10, 10)))
+//	println(exe.argDict)
+//	println(exe.gradDict)
+//
+//
+//	exe.forward(true, ("a",e1), ("f2",r1), ("f3",r2))
+//	println(exe.outputs(0).shape)
+//	println(exe.outputs(0).toArray.mkString(" "))
+//	exe.backward(outGrads = Array(de1, dr1, dr2))
+//	println(dr1.toArray.mkString(" "))
+//	println(r1.toArray.mkString(" "))
+//	println(r1.toArray(0) - dr1.toArray(0))
+
+//	exe.
+
+	println(op.getClass())
+	println(op.argDict)
+	println(op.gradDict)
 }
+
+
--- src/main/scala/SANSA/FactorGraph.scala
+++ src/main/scala/SANSA/FactorGraph.scala
@@ -5,6 +5,7 @@ import ml.dmlc.mxnet.{Symbol => s}
 import ml.dmlc.mxnet.{NDArray => nd}
 
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{Map => mutableMap}
 
 import SANSA.graph.{Factor => f, Variable => v}
 
@@ -17,21 +18,23 @@ import SANSA.graph.{Factor => f, Variable => v}
   * as described in the algorithm in the Tensorlog paper (https://arxiv.org/pdf/1605.06523.pdf)
   *
   **/
-class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) {
+class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f, _n_e: Int) {
 
 	/*
 	Copy the parameters into internal variables.
 	*/
 
+	private val numberEntities = _n_e
+
+
 	// Create an empty variable to be used for elegancy.
-	private val blank_node = new v(_is_head = false, _is_blank = true, _u = null, _label = "phi")
-
+	private val blankNode = new v(_is_head = false, _is_blank = true, _u = null, _label = "phi")
 	// Keep the list of variables in order to map the indices of the matrix to actual graph nodes
-	private val variables = _variables :+ blank_node
-	private val factors = _factors_body
+	private val variables = _variables :+ blankNode
 
+	private val factors = _factors_body
 	// Keep the predicate that appeared in the head of the query in order to know where do we start the BP
-	private val head_factor = _factor_head
+	private val headFactor = _factor_head
 
 	private def getNeighbors(node: v, exclude: f): Seq[f] = {
 		/*
@@ -58,7 +61,27 @@ class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) 
 		neighbors
 	}
 
-	def beliefPropagation(): Symbol = {
+	private def getSymbol(_label: String): s = {
+		/*
+			Use this function to get any symbol referred with a string.
+			Usecase: finding symbols fitting the listArguments() output on our desired symbolic expression.
+		 */
+		for (variable <- variables) {
+			if (variable.label.equals(_label)) {
+				return variable.u
+			}
+		}
+
+		for (factor <- factors) {
+			if (factor.label.equals(_label)) {
+				return factor.M
+			}
+		}
+
+		return null
+	}
+
+	def beliefPropagation(): Executor = {
 		/*
 		  Call this function to receive expression achieved by running the belief propagation algorithm.
 				We implement the algorithm listed in the paper mentioned in the comments above.
@@ -71,14 +94,52 @@ class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) 
 					-> Start with the o variable in head_factor,
 					-> Call compile_message_node on it.
 					-> Let the recursive functions do their thing and finally catch the composed expression
-					-> Bind the expression to a function and throw it back.
+					-> Do a softmax over this expression
+					-> Implement a cross entropy loss
+						-> requires a new defined symbolic variable
+					-> Implement square cost (or any other) over it.
+					-> Bind this cost expression (try defining what to pull the gradients over, and what to ignore)
+					-> Throw the executor back.
 		 */
 
-		val output = compileMessage_variable(head_factor.o, head_factor)
-		output
+		//Receive the equation from Belief Propagation.
+		var y = compileMessage_variable(headFactor.o, headFactor)
+
+		//Declare a new symbol, to represent the true output
+		var y_cap = s.Variable("true_output", Map("type" -> "variable"))
+
+		//Using softmax(y) and y_cap, compute cross entropy loss
+//		var loss = s.softmax_cross_entropy(name = "loss")()(Map("data" -> y, "label" -> y_cap))
+
+		//Compute the cost
+		var cost = s.LinearRegressionOutput(name = "cost")()(Map("data" -> y, "label" -> y_cap))
+
+		//To create the bind, we need a bit of shape inferences.
+			//First, programatically create a map of every factor name and its shape
+		var map = mutableMap[String, Shape]()
+		for (label <- cost.listArguments()) {
+			var symbol = getSymbol(label)
+			if (symbol != null) {
+				//If the symbol is a variable
+				if (symbol.attr("type") == Some("variable")) {
+					map += (label -> Shape(1, numberEntities))
+				}
+				else if (symbol.attr("type") == Some("factor")) {
+					map += (label -> Shape(numberEntities, numberEntities))
+				}
+			}
+
+		}
+		map += ("true_output" -> Shape(1,numberEntities))
+
+			//Then use that map to bind the cost symbol.
+		//@TODO: See which keyword best suites our purposes on gradReq
+		var executor = cost.simpleBind(ctx = Context.cpu(), gradReq = "write", shapeDict = map.toMap)
+
+		executor
 	}
 
-	private def compileMessage_variable(_node: v, _factor: f): Symbol = {
+	private def compileMessage_variable(_node: v, _factor: f): s = {
 		/*
 		  Pseudocode:
 		  (treat _node as X and _factor as L )
@@ -96,16 +157,16 @@ class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) 
 		    return v_x
 		 */
 
-		if ( _node == head_factor.i )  return _node.u
+		if ( _node == headFactor.i )  return _node.u
 
 
 		// Get the neighbors of this node, except for the factor given in the args
 		val neighbors = getNeighbors(_node, exclude = _factor)
 
 		// Send the neighbour + current node to compilemessage_factor and collect what they have to say.
-		var neighbor_values = new ListBuffer[Symbol]()
+		var neighbor_values = new ListBuffer[s]()
 		for (factor <- neighbors) {
-			var temp: Symbol = compileMessage_factor(factor, _node)
+			var temp: s = compileMessage_factor(factor, _node)
 			neighbor_values.+=(temp)
 		}
 
@@ -113,7 +174,7 @@ class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) 
 		/*
 			This code block does elementwise multiplication of all the values received in neighbor_values
 		*/
-		var v_x: Symbol = null
+		var v_x: s = null
 		try {
 			v_x = neighbor_values(0)
 		} catch {
@@ -125,15 +186,15 @@ class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) 
 		}
 
 		// For the rest of the symbols (if something still remains), do an elementwise multiplication to v_x
-		for (s <- neighbor_values.slice(1,neighbor_values.length)) {
-			v_x = v_x * s
+		for (symbol <- neighbor_values.slice(1,neighbor_values.length)) {
+			v_x = v_x * symbol
 		}
 
 		//Return it.
 		v_x
 	}
 
-	private def compileMessage_factor(_factor: f, _node: v): Symbol = {
+	private def compileMessage_factor(_factor: f, _node: v): s = {
 		/*
 		Pseudocode:
 			(treat _node as X and _factor as L)
@@ -149,7 +210,7 @@ class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) 
 
 			return this
 		*/
-		var v_i: Symbol = null
+		var v_i: s = null
 
 		//@TODO: See if we need another variable like self.v to represent the value of unary predicates.
 		if (_factor.o == null)  return _factor.M                                            // If the factor is unary.
@@ -159,6 +220,6 @@ class FactorGraph(_variables: List[v], _factors_body: List[f], _factor_head: f) 
 
 		else v_i = compileMessage_variable(_factor.o, _factor)                              // If the node is the input node for this factor
 
-		return Symbol.dot("dot")()(Map("lhs" -> v_i, "rhs" -> _factor.M))
+		return s.dot("dot")()(Map("lhs" -> v_i, "rhs" -> _factor.M))
 	}
 }
